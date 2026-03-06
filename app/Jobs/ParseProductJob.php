680b87313d68690632a6c0e110ce4166ac4bdd30
letoceiling-coder <dsc-23@yandex.ru>
2026-03-06 18:11:50 +0300

    Add ParseProductJob diagnostics

--- app/Jobs/ParseProductJob.php
+++ app/Jobs/ParseProductJob.php
@@ -46,14 +46,26 @@ class ParseProductJob implements ShouldQueue
         $saveDetails = $this->options['save_details'] ?? true;
         $savePhotos = $this->options['save_photos'] ?? false;
 
+        $externalId = $this->productData['external_id'] ?? $this->productData['id'] ?? null;
+        Log::info('ParseProductJob started', ['external_id' => $externalId]);
+
         $service = new DatabaseParserService($job);
-        $service->saveProductFromListing(
+        $result = $service->saveProductFromListing(
             $this->productData,
             $category,
             $saveDetails,
             $savePhotos,
             true
         );
+
+        Log::info('Product save result', [
+            'external_id' => $externalId,
+            'saved' => $result,
+        ]);
+
+        if ($result === false) {
+            Log::warning('Product skipped', ['external_id' => $externalId]);
+        }
 
         ParserLog::write('debug', 'ParseProductJob completed', [
             'product_external_id' => $this->productData['id'] ?? null,
